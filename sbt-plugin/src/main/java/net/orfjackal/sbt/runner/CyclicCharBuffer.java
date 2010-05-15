604e51f54525132585dc39731cde2383b1d46693
Esko Luontola <esko.luontola@gmail.com>
2010-05-15 21:31:30 +0300

    CyclicCharBuffer

---
+++ sbt-plugin/src/main/java/net/orfjackal/sbt/runner/CyclicCharBuffer.java
@@ -0,0 +1,69 @@
+// Copyright © 2010, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package net.orfjackal.sbt.runner;
+
+public class CyclicCharBuffer {
+
+    private final char[] buffer;
+    private int start = 0;
+    private int length = 0;
+
+    public CyclicCharBuffer(int capacity) {
+        buffer = new char[capacity];
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public char charAt(int i) {
+        return buffer[index(i)];
+    }
+
+    private int index(int i) {
+        return (start + i) % buffer.length;
+    }
+
+    public void append(char c) {
+        if (isFull()) {
+            removeFirst();
+        }
+        addLast(c);
+    }
+
+    private boolean isFull() {
+        return length == buffer.length;
+    }
+
+    private void removeFirst() {
+        start++;
+        length--;
+    }
+
+    private void addLast(char c) {
+        buffer[index(length)] = c;
+        length++;
+    }
+
+    public boolean contentEquals(String s) {
+        if (s.length() != length()) {
+            return false;
+        }
+        for (int i = 0; i < length(); i++) {
+            if (s.charAt(i) != charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder(buffer.length);
+        for (int i = 0; i < length(); i++) {
+            s.append(charAt(i));
+        }
+        return s.toString();
+    }
+}
---
+++ sbt-plugin/src/test/java/net/orfjackal/sbt/runner/CyclicCharBufferTest.java
@@ -0,0 +1,68 @@
+// Copyright © 2010, Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package net.orfjackal.sbt.runner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CyclicCharBufferTest {
+
+    private static final int UNUSED_CAPACITY = 100;
+
+    @Test
+    public void initially_the_buffer_is_empty() {
+        CyclicCharBuffer buffer = new CyclicCharBuffer(UNUSED_CAPACITY);
+
+        assertHasString("", buffer);
+    }
+
+    @Test
+    public void the_buffer_contains_what_is_written_there() {
+        CyclicCharBuffer buffer = new CyclicCharBuffer(UNUSED_CAPACITY);
+        buffer.append('a');
+        buffer.append('b');
+
+        assertHasString("ab", buffer);
+    }
+
+    @Test
+    public void the_first_chars_are_dropped_when_the_buffer_is_filled() {
+        CyclicCharBuffer buffer = new CyclicCharBuffer(3);
+        buffer.append('a');
+        buffer.append('b');
+        buffer.append('c');
+
+        assertHasString("abc", buffer);
+
+        buffer.append('d');
+
+        assertHasString("bcd", buffer);
+    }
+
+    @Test
+    public void can_test_whether_the_buffer_content_equals_a_string() {
+        CyclicCharBuffer buffer = new CyclicCharBuffer(3);
+        buffer.append('a');
+        buffer.append('b');
+
+        assertTrue("should equal <ab>", buffer.contentEquals("ab"));
+
+        for (String s : new String[]{
+                "a",    // shorter than length
+                "ax",   // different content
+                "abc",  // longer than length
+                "abcd", // longer than capacity
+        }) {
+            assertFalse("should not equal <" + s + ">", buffer.contentEquals(s));
+        }
+    }
+
+    private static void assertHasString(String expected, CyclicCharBuffer actual) {
+        assertEquals("size", expected.length(), actual.length());
+        assertEquals("content", expected, actual.toString());
+        assertTrue("contentEquals", actual.contentEquals(expected));
+    }
+}
