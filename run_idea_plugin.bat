8e0375844d021d2a6de9accdf0ae2898be72a5e8
Esko Luontola <esko.luontola@gmail.com>
2010-10-25 18:26:40 +0300

    Upgrade to IDEA 9.0.3

--- run_idea_plugin.bat
+++ run_idea_plugin.bat
@@ -5,4 +5,4 @@ call rmdir /s /q "%USERPROFILE%\.IntelliJIdea90-sandbox\config\plugins\SBT"
 call "C:\Program Files\7-Zip\7z.exe" x -y -o"%USERPROFILE%\.IntelliJIdea90-sandbox\config\plugins" sbt-dist/target/idea-sbt-plugin-*.zip
 
 set IDEA_PROPERTIES=%~dp0\idea.properties
-call "C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 9.0.2\bin\idea.exe"
+call "C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 9.0.3\bin\idea.exe"
