47b05d7027b9b212c683234c5573d9366fe91bc2
Esko Luontola <esko.luontola@gmail.com>
2011-04-21 21:37:37 +0300

    Removed the .bat file for launching IDEA in a sandbox, because it works well enough from within IDEA

--- run_idea_plugin.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call mvn clean package
-
-call rmdir /s /q "%USERPROFILE%\.IntelliJIdea90-sandbox\config\plugins\SBT"
-call "C:\Program Files\7-Zip\7z.exe" x -y -o"%USERPROFILE%\.IntelliJIdea90-sandbox\config\plugins" sbt-dist/target/idea-sbt-plugin-*.zip
-
-set IDEA_PROPERTIES=%~dp0\idea.properties
-call "C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 9.0.3\bin\idea.exe"
